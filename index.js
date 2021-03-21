17b49348b18e87c603929cee75ee6eca2c4307dc
sagibarnabas <sagibarnabas@yahoo.com>
2021-03-21 11:59:34 +0100

    helo

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var app = express();
 
 app.use(express.static('static'));
 require('./routes/outside')(app);
-console.log("öccá");
 var server = app.listen(3000, function () {
     console.log("On: 3000");
 });
