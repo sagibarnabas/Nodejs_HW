69c42e783b615037bdf0a41d53531a99827938d6
sagibarnabas <sagibarnabas@yahoo.com>
2021-03-21 10:47:16 +0100

    Initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static('static'));
 require('./routes/outside')(app);
-console.log('helo');
+console.log('hasd');
 var server = app.listen(3000, function () {
     console.log("On: 3000");
 });
