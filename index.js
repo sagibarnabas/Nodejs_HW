6146cd4b69d0a83762b84b7f76f41af843221b1d
sagibarnabas <sagibarnabas@yahoo.com>
2021-03-21 11:27:23 +0100

    Merge remote-tracking branch 'main/main' into main

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('static'));
-console.log("helo");
+console.log("jelo");
 require('./routes/outside')(app);
 var server = app.listen(3000, function () {
     console.log("On: 3000");
