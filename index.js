c05d778e5ee9bad72c24fde37863399680726d46
sagibarnabas <sagibarnabas@yahoo.com>
2021-03-21 16:44:25 +0100

    ads

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var app = express();
 app.set('view engine','ejs');
 app.use(express.static('static'));
 require('./routes/outside')(app);
+console.log("helo");
 var server = app.listen(3000, function () {
     console.log("On: 3000");
 });
