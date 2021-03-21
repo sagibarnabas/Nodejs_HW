0febf8bad1c895e2919371fe8b88fcb469470c86
sagibarnabas <sagibarnabas@yahoo.com>
2021-03-21 11:27:53 +0100

    helo

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('static'));
-console.log("jelo");
 require('./routes/outside')(app);
 var server = app.listen(3000, function () {
     console.log("On: 3000");
