a59c1978aae003a2b2572b3da6a4904a2999b932
sagibarnabas <sagibarnabas@yahoo.com>
2021-03-22 21:06:34 +0100

    yes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('view engine','ejs');
 app.use(express.static('static'));
-console.log("helo");
+console.log("helokakaka");
 require('./routes/outside')(app);
 var server = app.listen(3000, function () {
     console.log("On: 3000");
