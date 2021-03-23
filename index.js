b2037e580fa3c5748ac054fd4e109d8c668099df
sagibarnabas <sagibarnabas@yahoo.com>
2021-03-23 19:34:09 +0100

    helo

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var app = express();
 
 app.set('view engine','ejs');
 app.use(express.static('static'));
-console.log("helokakaka");
 require('./routes/outside')(app);
 var server = app.listen(3000, function () {
     console.log("On: 3000");
